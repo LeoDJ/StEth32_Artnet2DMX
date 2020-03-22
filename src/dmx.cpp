1ba344678932745617154ee0882bd30353b2fb7f
LeoDJ <leandro.spaeth@gmail.com>
2020-03-22 15:25:06 +0100

    use timer to trigger DMX transmission

--- src/dmx.cpp
+++ src/dmx.cpp
@@ -8,6 +8,8 @@
 void MX_TIM4_Init(void);
 void sendDMX();
 
+HardwareTimer dmxTimer(TIM4);
+
 uint8_t dmxBufs[3][513] = {{0}};
 // uint8_t dmxOutBuf[3][513] = {{0}}; // ToDo: maybe double buffering, if necessary?
 
@@ -17,13 +19,11 @@ uint8_t dmxDePins[] =               {PB1,       PA4,        PA6     };
 uint8_t numDmxUarts = sizeof(dmxUarts) / sizeof(dmxUarts[0]);
 
 void setDmxData(uint8_t output, uint8_t* buf, uint16_t size) {
-    // Serial3.print('c');
     if(output >= 0 && output <= MAX_UNIVERSES) {
         if(size > DMX_CHANNELS_MAX) {
             size = DMX_CHANNELS_MAX;
         }
         memcpy(dmxBufs[output] + 1, buf, size);
-        sendDMX();
         // char tmp[100];
         // snprintf(tmp, 100, "DMX: %02X %02X %02X %02X\n", dmxBufs[output][1], dmxBufs[output][2], dmxBufs[output][3], dmxBufs[output][4]);
         // DEBUG.print(tmp);
@@ -34,26 +34,11 @@ inline uint8_t outputEnabled(uint8_t outputId) {
     return _config.outputs.raw & (1 << outputId);
 }
 
-uint32_t lastDmxSend = 0;
-
 void sendDMX() {
-    while(millis() - lastDmxSend < 23) {}
-    lastDmxSend = millis();
-
-    uint32_t time = micros();
-
-    // ensure long enough mark before next packet
-    for(uint8_t i = 0; i < numDmxUarts; i++) {
-        if(outputEnabled(i)) { 
-            pinMode(dmxTxPins[i], OUTPUT);
-            digitalWrite(dmxTxPins[i], HIGH); 
-        }
-    }
-    delayMicroseconds(20);
-
     // Space for break
     for(uint8_t i = 0; i < numDmxUarts; i++) {
         if(outputEnabled(i)) { 
+            pinMode(dmxTxPins[i], OUTPUT);
             digitalWrite(dmxTxPins[i], LOW); 
             // memcpy(dmxOutBuf[i], dmxBufs[i], 513);
         }
@@ -77,6 +62,10 @@ void sendDMX() {
     }
 }
 
+void sendDMX(HardwareTimer* ht) {
+    sendDMX();
+}
+
 void initDMX() {
     for(uint8_t i = 0; i < numDmxUarts; i++) {
         if(outputEnabled(i)) { 
@@ -92,71 +81,11 @@ void initDMX() {
     MX_USART2_UART_Init();
     MX_USART3_UART_Init();
 
-    // TODO: fix timer
-    MX_TIM4_Init();
-    HAL_NVIC_SetPriority(TIM4_IRQn, 1, 1);
-    HAL_NVIC_EnableIRQ(TIM4_IRQn);
-    __HAL_RCC_TIM4_CLK_ENABLE();
+    dmxTimer.attachInterrupt(sendDMX);
+    dmxTimer.setPrescaleFactor(72); // 1MHz
+    dmxTimer.setOverflow(23000);    // 1MHz / 23000us ~= 43.5Hz
+    dmxTimer.resume();
 }
 
-// workaround until timer is fixed
-uint32_t lastSend = 0;
 void loopDMX() {
-    if(millis() - lastSend >= 30) { // 40 Hz
-        lastSend = millis();
-        // sendDMX();
-    } 
-}
-
-TIM_HandleTypeDef htim4;
-
-void TIM4_IRQHandler(void)
-{
-    HAL_TIM_IRQHandler(&htim4);
-    DEBUG.print("DMX Timer: ");
-    DEBUG.println(millis());
-    //sendDMX();
-}
-
-
-void MX_TIM4_Init(void)
-{
-    TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-    TIM_MasterConfigTypeDef sMasterConfig = {0};
-    TIM_OC_InitTypeDef sConfigOC = {0};
-
-    htim4.Instance = TIM4;
-    htim4.Init.Prescaler = 7199; // 10kHz
-    htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim4.Init.Period = 199; //10kHz / 200 = 50Hz
-    htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-    htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-    if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-    if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    if (HAL_TIM_OC_Init(&htim4) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-    if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    sConfigOC.OCMode = TIM_OCMODE_TIMING;
-    sConfigOC.Pulse = 0;
-    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-    if (HAL_TIM_OC_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-    {
-        Error_Handler();
-    }
-
 }
